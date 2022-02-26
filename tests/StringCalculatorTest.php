d8d81590377134ec02d352b210b743506a0d2df6
jalex3421 <alemt1708@hotmail.com>
2022-02-26 20:30:14 +0000

    Commit 16: general_refactor

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -11,11 +11,18 @@ use PHPUnit\Framework\TestCase;
 class StringCalculatorTest  extends TestCase{
 
     /**
+     * @setUp
+     */
+    protected function setUp():void{
+        parent::setUp();
+        $this->stringCalculator=  new StringCalculator();
+    }
+
+    /**
      * @test : check if string is empty
      */
     public function string_is_empty(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->calculateEmpty("");
+        $res = $this->stringCalculator->calculateEmpty("");
         $this->assertEquals("0",$res); //assert
     }
 
@@ -23,8 +30,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : check if string is not empty
      */
     public function string_is_not_empty(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->calculateNotEmpty("");
+        $res = $this->stringCalculator->calculateNotEmpty("abcd");
         $this->assertNotEmpty($res); //assert
     }
 
@@ -32,8 +38,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : return float value from string
      */
     public function string_to_int(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->calculateStringToFloat("1.1");
+        $res = $this->stringCalculator->calculateStringToFloat("1.1");
         $this->assertIsFloat($res); //assert
     }
 
@@ -41,8 +46,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : return sum of a given sequence , delimitator:  comma
      */
     public function add(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->calculateAdd("1.1,2,3,4.1,5");
+        $res = $this->stringCalculator->calculateAdd("1.1,2,3,4.1,5");
         $this->assertIsFloat($res);
     }
 
@@ -50,8 +54,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : return sum of a given sequence , delimitator: new line or comma
      */
     public function add_new_line(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->calculateAddNewLine("1\n2,3");
+        $res = $this->stringCalculator->calculateAddNewLine("1\n2,3");
         $this->assertEquals(6,$res);
     }
 
@@ -59,8 +62,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : doesnt allow new line in final position
      */
     public function add_new_line_restrictor(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->calculateAddNewLineRestrictor("175.2,\n35");
+        $res = $this->stringCalculator->calculateAddNewLineRestrictor("175.2,\n35");
         $this->assertEquals("Number expected but newline found at position 6",$res);
     }
 
@@ -68,8 +70,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : doesnt allow that an input sequence ended with a separator
      */
     public function eof_new_line_restrictor(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->calculateAddEOFLineRestrictor("1,3,");
+        $res =$this->stringCalculator->calculateAddEOFLineRestrictor("1,3,");
         $this->assertEquals("Number expected but EOF found",$res);
     }
 
@@ -77,8 +78,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : allow to introduce your own separators
      */
     public function find_custom_separator_base(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->obtainCustomSeparators("//;\n1;3");
+        $res = $this->stringCalculator->obtainCustomSeparators("//;\n1;3");
         $this->assertEquals("Separator is ;",$res);
     }
 
@@ -86,8 +86,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : allow to introduce your own separators and verify the syntax of given input
      */
     public function find_custom_separator_restrictor(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->obtainCustomSeparatorsRestrictor("//sep\n3sep2");
+        $res = $this->stringCalculator->obtainCustomSeparatorsRestrictor("//sep\n3sep2");
         $this->assertEquals(5,$res);
     }
 
@@ -96,8 +95,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : doesnt allow one negative number in sequence
      */
     public function find_one_negative(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->obtainOneNegative("-1,2");
+        $res = $this->stringCalculator->obtainOneNegative("-1,2");
         $this->assertEquals("Negative not allowed: -1",$res);
     }
 
@@ -105,8 +103,7 @@ class StringCalculatorTest  extends TestCase{
      * @test : doesnt allow multiple negative numbers in sequence
      */
     public function find_multiple_negatives(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->obtainMultipleNegatives("2,-4,-5");
+        $res = $this->stringCalculator->obtainMultipleNegatives("2,-4,-5");
         $this->assertEquals("Negatives not allowed: -4-5",$res);
     }
 
@@ -114,18 +111,15 @@ class StringCalculatorTest  extends TestCase{
      * @test : trigger multiple errors if it is necessary
      */
     public function multiple_errors(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->obtainMultipleErrors("-1,,2");
+        $res = $this->stringCalculator->obtainMultipleErrors("-1,,2");
         $this->assertEquals("Negatives not allowed: -1\nNumber expected but , found",$res);
     }
-
 
     /**
      * @test : return product of a given sequence , delimitator:  comma
      */
     public function multiply(){
-        $stringCalculator = new StringCalculator();
-        $res = $stringCalculator->calculateProduct("7,7,2");
+        $res = $this->stringCalculator->calculateProduct("7,7,2");
         $this->assertEquals(98,$res);
     }
 
